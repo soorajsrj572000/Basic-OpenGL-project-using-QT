de945f120cc2ad3b4c6b496f39359102da203423
soorajsrj572000 <98225347+soorajsrj572000@users.noreply.github.com>
2025-02-25 12:06:49 +0530

    Creating a basic QT project

---
+++ OpenGL_Development/openglwidget.cpp
@@ -0,0 +1,26 @@
+#include "openglwidget.h"
+
+OpenGLWidget::OpenGLWidget(QWidget *parent)
+    : QOpenGLWidget(parent)
+{
+}
+
+OpenGLWidget::~OpenGLWidget()
+{
+}
+
+void OpenGLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void OpenGLWidget::resizeGL(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+void OpenGLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
